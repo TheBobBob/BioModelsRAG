f472f2c58455bf14a2d81989b97685e6ed0d5fe7
TheBobBob <bhavyahshree@gmail.com>
2024-09-08 16:48:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,35 +1,29 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='BioModelsRAG',
-    version='0.2.0',
-    packages=find_packages(),
-    include_package_data=True,
-    package_data={
-        'BioModelsRAG': ['data/*']
-    },
+    name='BioModelsRAG',  
+    version='0.3.1',            
+    packages=find_packages(), 
     install_requires=[
-        'ollama', 
-        'langchain', 
-        'chromadb', 
-        'sentence_transformers', 
+        'requests',                
+        'tellurium',               
+        'ollama',                  
         'langchain_text_splitters',
+        'chromadb',                
     ],
     entry_points={
         'console_scripts': [
-            'start = splitBiomodels:final_items'
+            'biomodels-cli = searchBioModels:main',  
         ],
     },
-    author='Bhavyahshree Navaneetha Krishnan',
-    author_email='bhavyak7@uw.edu',
-    description='BioModelsRAG is a tool to analyze the wide repository of BioModels that are available on the internet and ask specific questions about the BioModels. It is created to be an assistant to system biology researchers.',
-    long_description_content_type='text/markdown',
-    url='https://github.com/TheBobBob/BioModelsRAG',
+    author='Bhavyahshree Navaneetha Krishnan', 
+    author_email='your_email@example.com',     
+    description='A package for processing and querying biomodels', 
+    url='https://github.com/your_username/your_repo', 
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6', 
 )
-
